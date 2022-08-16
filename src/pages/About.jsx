0c86e59298f8e52f52ad258191a50ed210bd64ad
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-08-16 10:33:46 +0200

    work on about collapses and footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,9 @@ import Frame from "../components/Frame";
 import "../styles/normalize.css";
 import "../styles/pages/app.scss";
 import Footer from "../components/Footer";
-
-import { Link } from "react-router-dom";
+import aboutData from "../data/aboutData";
+import Collapse from "../components/Collapse";
+import "../styles/pages/About.scss";
 
 export default function About() {
   const highlight = {
@@ -13,13 +14,26 @@ export default function About() {
     about: true,
   };
 
+  const aboutCollapes = aboutData.map((element, index) => {
+    return (
+      <div className="about__collapse" key={index}>
+        <Collapse
+          title={element.title}
+          content={element.content}
+          listOfElement={false}
+        />
+      </div>
+    );
+  });
+
   return (
-    <>
+    <div className="about">
       <Frame>
         <Navbar highlight={highlight} />
         <Banner homepage={false} />
+        {aboutCollapes}
       </Frame>
       <Footer />
-    </>
+    </div>
   );
 }
