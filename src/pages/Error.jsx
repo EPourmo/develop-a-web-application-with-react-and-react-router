09e10145549ce3790d30d7113b84d88ed8bf0ebd
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-08-18 10:42:11 +0200

    remove footer from error page (not aesthetic)

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,30 +1,26 @@
 import Navbar from "../components/Navbar";
 import Frame from "../components/Frame";
-import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import "../styles/pages/Error.scss";
 
 export default function Error() {
   return (
-    <div className="error__page">
-      <Frame>
-        <Navbar
-          highlight={{
-            home: false,
-            about: false,
-          }}
-        />
-        <div className="error">
-          <h1 className="error__404">404</h1>
-          <p className="error__msg">
-            Oups! La page que vous demandez n'existe pas.
-          </p>
-          <Link to="/" className="error__link">
-            <p>Retourner sur la page d’accueil</p>
-          </Link>
-        </div>
-      </Frame>
-      <Footer />
-    </div>
+    <Frame>
+      <Navbar
+        highlight={{
+          home: false,
+          about: false,
+        }}
+      />
+      <div className="error">
+        <h1 className="error__404">404</h1>
+        <p className="error__msg">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link to="/" className="error__link">
+          <p>Retourner sur la page d’accueil</p>
+        </Link>
+      </div>
+    </Frame>
   );
 }
